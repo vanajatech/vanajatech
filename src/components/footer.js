53b467de48786b9ea565096b2b53fef61ce38937
MatiasPuhakka <matias.puhakka@gmail.com>
2020-06-03 17:14:39 +0300

    Use material-ui on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,60 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
 
-import { Row } from "./grid"
+// TODO; Try to reduce the amount of bloat this brings
+import Grid from "@material-ui/core/Grid"
+
+import { Github, Instagram, Linkedin } from "./icons"
 
 const Footer = () => (
-  <Row>
-    <div className="flex flex-1 justify-between px-4">
-      <div className="flex items-center text-gray-500 text-sm font-normal py-12">
-        <p className="text-sm text-gray-500">
-          © 2020 Vanaja Tech. All rights reserved.
-        </p>
-        <a className="ml-12">Privacy Policy</a>
-        <a className="ml-12">Tietosuojaseloste</a>
-      </div>
-    </div>
-  </Row>
+  <Grid container direction="row" justify="center" alignItems="center">
+    <Grid item xs>
+      <SocialIcons>
+        <a href="https://github.com/vanajatech">
+          <Github />
+        </a>
+        <a href="https://www.instagram.com/vanajatech/">
+          <Instagram />
+        </a>
+        <a href="https://www.linkedin.com/company/vanaja-tech/">
+          <Linkedin />
+        </a>
+      </SocialIcons>
+      <Links>
+        <Link to="/contact-us">Contact Us</Link>
+        <span> | </span>
+        <Link to="/privacy-policy">Privacy Policy</Link>
+        <span> | </span>
+        <Link to="/tietosuojaseloste">Tietosuojaseloste</Link>
+      </Links>
+      <Copyright>© 2020 Vanaja Tech. All rights reserved.</Copyright>
+    </Grid>
+  </Grid>
 )
+
+const SocialIcons = styled.div`
+  color: var(--gray-900);
+  display: flex;
+  gap: 3rem;
+  justify-content: center;
+  padding-top: 3rem;
+`
+
+const Links = styled.div`
+  color: var(--gray-500);
+  text-align: center;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+`
+
+const Copyright = styled.p`
+  color: var(--gray-500);
+  font-size: 0.875rem;
+  display: flex;
+  justify-content: center;
+  padding-bottom: 3rem;
+`
 
 export default Footer
