5b85916cfd4a01da690df1e6f8fdfd9a33b9923b
MatiasPuhakka <matias.puhakka@gmail.com>
2020-06-05 15:32:35 +0300

    Style the button with styled-components

--- src/components/button.js
+++ src/components/button.js
@@ -1,10 +1,23 @@
-import React from "react"
+import styled from "styled-components"
 
-const Button = ({ ...props }) => (
-  <button
-    className="bg-red text-white font-mono py-3 px-6 rounded shadow-red"
-    {...props}
-  />
-)
+export const Button = styled.button`
+  background: var(--primary);
+  color: #fff;
+  font-weight: 700;
+  line-height: 1rem;
+  padding: 0.75rem 1.5rem;
+  border-radius: 4px;
+  box-shadow: 2px 2px 5px rgba(180, 10, 10, 0.24);
+  transition: all 0.2s ease-in-out;
+
+  &:hover,
+  &:focus {
+    background: #ff1414;
+  }
+
+  &:active {
+    background: #e10000;
+  }
+`
 
 export default Button
