e9412933571f502b816f1a06f5ab03ca6cab7deb
MatiasPuhakka <matias.puhakka@gmail.com>
2020-06-05 13:26:27 +0300

    Our Work page progress

--- src/pages/our-work.js
+++ src/pages/our-work.js
@@ -1,30 +1,107 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import Button from "../components/button"
-import { Row, Col } from "../components/grid"
-import { Card, CardHeader } from "../components/card"
-import { Input } from "../components/input"
-
 const OurWork = () => (
   <Layout>
     <SEO title="Our Work" />
-    <Row>
-      <Col className="text-center">
-        <h1>Our Work</h1>
-        <p>
-          Chambray plaid edison bulb, church-key occupy you probably haven't
-          heard of them snackwave pok pok. Vexillologist prism authentic
-          pour-over jean shorts. Pok pok artisan everyday carry food truck,
-          messenger bag hoodie offal bespoke four loko.
-        </p>
-        <Link to="/">Go back home</Link>
-      </Col>
-    </Row>
+
+    <Grid container direction="column" justify="flex-start" alignItems="center">
+      <Grid item md={8} xs={12}>
+        <Box textAlign="center" m="9rem 0 6rem">
+          <h1>Our Work</h1>
+          <p>
+            Chambray plaid edison bulb, church-key occupy you probably haven't
+            heard of them snackwave pok pok. Vexillologist prism authentic
+            pour-over jean shorts. Pok pok artisan everyday carry food truck,
+            messenger bag hoodie offal bespoke four loko.
+          </p>
+        </Box>
+      </Grid>
+    </Grid>
+
+    <Grid container direction="column" justify="flex-start" alignItems="center">
+      <Grid item md={6} xs={12}>
+        <Grid container wrap="nowrap" style={{ marginBottom: "4rem" }}>
+          <Grid item>
+            <BigNumber>1</BigNumber>
+          </Grid>
+          <Grid item xs zeroMinWidth>
+            <h3>Business Critical Applications</h3>
+            <p>
+              We promise self-confidently we can implement the most
+              business-critical applications. We’ve already written code to pay
+              invoices, transfer money and create an audit trail.
+            </p>
+            <p>Tell us what you need; we’ll dare to implement it.</p>
+          </Grid>
+        </Grid>
+        <Grid container wrap="nowrap" style={{ marginBottom: "4rem" }}>
+          <Grid item>
+            <BigNumber>2</BigNumber>
+          </Grid>
+          <Grid item xs zeroMinWidth>
+            <h3>Specialty: Business Services and Finance</h3>
+            <p>
+              Chambray plaid edison bulb, church-key occupy you probably haven't
+              heard of them snackwave pok pok. Vexillologist prism authentic
+              pour-over jean shorts.
+            </p>
+            <p>
+              Pok pok artisan everyday carry food truck, messenger bag hoodie
+              offal bespoke four loko.
+            </p>
+          </Grid>
+        </Grid>
+        <Grid container wrap="nowrap" style={{ marginBottom: "4rem" }}>
+          <Grid item>
+            <BigNumber>3</BigNumber>
+          </Grid>
+          <Grid item xs zeroMinWidth>
+            <h3>Boldly Self-Confident</h3>
+            <p>We work efficiently and decisively.</p>
+            <p>
+              Instead of contemplating what to do, we approach problems head on
+              and progress to pick the right way to proceed.
+            </p>
+          </Grid>
+        </Grid>
+        <Grid container wrap="nowrap" style={{ marginBottom: "4rem" }}>
+          <Grid item>
+            <BigNumber>4</BigNumber>
+          </Grid>
+          <Grid item xs zeroMinWidth>
+            <h3>JavaScript-Oriented Generalists</h3>
+            <p>We use the best technology for each project</p>
+            <p>
+              But if we can do it in JavaScript, we will. The JavaScript
+              ecosystem is growing fast and it can be ubiquitously used in
+              servers, websites and mobile apps.
+            </p>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   </Layout>
 )
+
+const BigNumber = styled.div`
+  display: inline;
+  text-align: right;
+  vertical-align: top;
+  color: rgba(180, 10, 10, 0.6);
+  font-family: "IBM Plex Mono";
+  font-weight: bold;
+  font-size: 144px;
+  line-height: 110px;
+  letter-spacing: 0.03em;
+  margin-right: 2rem;
+`
 
 export default OurWork
