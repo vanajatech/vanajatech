0cbed7ecbcd1825d88200565f2bdf44dc07154f5
MatiasPuhakka <matias.puhakka@gmail.com>
2020-06-16 11:19:39 +0300

    Remove unused pages and links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,13 @@
-import { Link } from "gatsby"
 import React from "react"
+// import { Link } from "gatsby"
 import styled from "styled-components"
-
-// TODO; Try to reduce the amount of bloat this brings
 import Grid from "@material-ui/core/Grid"
 
-import { Github, Instagram, Linkedin } from "./icons"
+import {
+  Github,
+  // Instagram,
+  Linkedin,
+} from "./icons"
 
 const Footer = () => (
   <Grid container className="mt-24">
@@ -14,20 +16,20 @@ const Footer = () => (
         <a href="https://github.com/vanajatech">
           <Github />
         </a>
-        <a href="https://www.instagram.com/vanajatech/">
+        {/* <a href="https://www.instagram.com/vanajatech/">
           <Instagram />
-        </a>
+        </a> */}
         <a href="https://www.linkedin.com/company/vanaja-tech/">
           <Linkedin />
         </a>
       </SocialIcons>
-      <Links>
+      {/* <Links>
         <Link to="/contact-us">Contact Us</Link>
         <span> | </span>
         <Link to="/privacy-policy">Privacy Policy</Link>
         <span> | </span>
         <Link to="/tietosuojaseloste">Tietosuojaseloste</Link>
-      </Links>
+      </Links> */}
       <Copyright>© 2020 Vanaja Tech. All rights reserved.</Copyright>
     </Grid>
   </Grid>
@@ -44,19 +46,20 @@ const SocialIcons = styled.div`
   }
 `
 
-const Links = styled.div`
-  color: var(--gray-900);
-  text-align: center;
-  padding-top: 2rem;
-  padding-bottom: 2rem;
-`
+// const Links = styled.div`
+//   color: var(--gray-900);
+//   text-align: center;
+//   margin-top: 2rem;
+//   margin-bottom: 2rem;
+// `
 
 const Copyright = styled.p`
   color: var(--gray-900);
   font-size: 0.875rem;
   display: flex;
   justify-content: center;
-  padding-bottom: 4rem;
+  margin-top: 2rem;
+  margin-bottom: 4rem;
 `
 
 export default Footer
