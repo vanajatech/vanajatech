cbe3914bf55fcf063ed9aab120d8c3fb3e8ee0f6
MatiasPuhakka <matias.puhakka@gmail.com>
2020-06-18 15:00:53 +0300

    Remove placeholder google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,18 +79,5 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-169924981-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-      },
-    },
   ],
 }
