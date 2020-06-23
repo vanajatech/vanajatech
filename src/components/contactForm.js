cad445d2a3961eeb36afb34e09ed3b2cbee05bd7
MatiasPuhakka <matias.puhakka@gmail.com>
2020-06-23 11:17:23 +0300

    Fix form

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -19,7 +19,7 @@ const ContactForm = ({ name, title, submitText }) => (
           action="/thank-you/"
         >
           <input type="hidden" name="bot-field" />
-          <input type="hidden" name={name} value="contact" />
+          <input type="hidden" name="form-name" value={name} />
           <label htmlFor="email" className="hidden">
             E-mail
           </label>
